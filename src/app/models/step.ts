1f81c1359d72419ff8823e0f0cd083f6a631bf9f
John Alexander Quintero Berrío <jaquinterob@hotmail.com>
2024-01-18 16:00:06 -0500

    success gif was added

--- src/app/models/step.ts
+++
@@ -1,8 +0,0 @@
-export interface Step {
-  isTheNext: boolean;
-  done: boolean;
-  type: StepType;
-}
-
-export type StepType = 'punishable' | 'innocent';
-export type ButtonPressed = 'good' | 'bad' | '';
